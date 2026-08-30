67aaa069028e227efe8f1c45d8e537721362b1b3
Mark Russell <markisrt4@gmail.com>
2026-08-30 12:38:58 -0400

    Add lifecycle-aware camera preview surface

---
+++ app/src/main/java/org/openroadcode/androidbridge/CameraPreviewView.java
@@ -0,0 +1,49 @@
+package org.openroadcode.androidbridge;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Lightweight in-app preview surface for the camera stream.
+ *
+ * The view never opens the camera itself. CameraStreamService remains the sole
+ * Camera2 owner and adds/removes this surface from its existing capture session.
+ */
+final class CameraPreviewView extends SurfaceView implements SurfaceHolder.Callback {
+    private static final int PREVIEW_WIDTH = 16;
+    private static final int PREVIEW_HEIGHT = 9;
+
+    CameraPreviewView(Context context) {
+        super(context);
+        setBackgroundColor(Color.BLACK);
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        if (width <= 0) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        int height = width * PREVIEW_HEIGHT / PREVIEW_WIDTH;
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        CameraStreamService.setPreviewSurface(holder.getSurface());
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        CameraStreamService.setPreviewSurface(holder.getSurface());
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        CameraStreamService.clearPreviewSurface(holder.getSurface());
+    }
+}
