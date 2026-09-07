660630e79a3fb3296929417f517367acf0d1de70
Mark Russell <markisrt4@gmail.com>
2026-09-07 07:41:56 -0400

    Generalize Bluetooth vehicle provider naming

--- app/src/main/java/org/openroadcode/androidbridge/config/ServiceProvider.java
+++ app/src/main/java/org/openroadcode/androidbridge/config/ServiceProvider.java
@@ -3,7 +3,7 @@ package org.openroadcode.androidbridge.config;
 public enum ServiceProvider {
     ANDROID_SENSORS("Android Sensors"),
     SIMULATED_DRIVE("Simulated Drive"),
-    KONNWEI_SPP("KONNWEI SPP"),
+    BLUETOOTH_SPP("Bluetooth OBD-II (SPP)"),
     SIMULATED_VEHICLE("Simulated Vehicle");
 
     private final String displayName;
