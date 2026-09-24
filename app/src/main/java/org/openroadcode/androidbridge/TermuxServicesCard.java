95c8b8478dfbddcb37216a0d0ef20c7896776c47
Mark Russell <markisrt4@gmail.com>
2026-09-24 08:41:44 -0400

    Refresh bridge registration for remote runtime

--- app/src/main/java/org/openroadcode/androidbridge/TermuxServicesCard.java
+++ app/src/main/java/org/openroadcode/androidbridge/TermuxServicesCard.java
@@ -151,7 +151,11 @@ public final class TermuxServicesCard {
 
   public void start() {
     handler.removeCallbacks(refreshTask);
-    handler.post(refreshTask);
+    if (settings.target() == Target.REMOTE_PI && settings.hasRemotePiConfiguration()) {
+      runAction(RuntimeServiceManagerClient::registerAndroidBridge);
+    } else {
+      handler.post(refreshTask);
+    }
   }
 
   public void stop() { handler.removeCallbacks(refreshTask); }
